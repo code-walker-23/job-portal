3fdf940961573e846065b7f4f08b9d4b5d5b3be9
code-walker-23 <chhabravinay549@gmail.com>
2024-10-04 13:37:53 +0530

    routing-setup-completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,52 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
-import { Button } from "./components/ui/button";
+import AppLayout from "./layout/AppLayout";
+import LandingPage from "./pages/LandingPage";
+import OnBoarding from "./pages/OnBoarding";
+import Job from "./pages/Job";
+import JobListing from "./pages/JobListing";
+import PostJob from "./pages/PostJob";
+import SavedJobs from "./pages/SavedJobs";
+import MyJobs from "./pages/MyJobs";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: <OnBoarding />,
+      },
+      {
+        path: "/jobs",
+        element: <JobListing />,
+      },
+      {
+        path: "/job/:id",
+        element: <Job />,
+      },
+      {
+        path: "/post-job",
+        element: <PostJob />,
+      },
+      {
+        path: "/saved-jobs",
+        element: <SavedJobs />,
+      },
+      {
+        path: "/my-jobs",
+        element: <MyJobs />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className=" text-white font-medium">
-      <Button> Vinay Chhabra </Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/layout/AppLayout.jsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+    </div>
+  );
+};
+
+export default AppLayout;
