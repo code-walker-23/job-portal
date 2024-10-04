1830dab0b2144c286fa80b9a2385ccf15bbfc926
code-walker-23 <chhabravinay549@gmail.com>
2024-10-04 12:56:57 +0530

    project-setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className=" text-white font-medium">
+      <Button> Vinay Chhabra </Button>
+    </div>
+  );
+}
+
+export default App;
