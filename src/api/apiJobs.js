acffba71766bbc7cb15250d214b40ab761500eef
code-walker-23 <chhabravinay549@gmail.com>
2024-10-05 21:10:58 +0530

    building-job-page

--- src/api/apiJobs.js
+++ src/api/apiJobs.js
@@ -56,3 +56,19 @@ export async function saveJob(token, { alreadySaved }, savedData) {
     return data;
   }
 }
+
+export async function getSingleJob(token, { job_id }) {
+  const supabase = await supabaseClient(token);
+
+  const { data, error } = await supabase
+    .from("jobs")
+    .select("*, company: companies(name,logo_url),applications:applications(*)")
+    .eq("id", job_id)
+    .single();
+
+  if (error) {
+    console.error("Error Fetching Single Job:", error);
+    return null;
+  }
+  return data;
+}
--- src/pages/Job.jsx
+++ src/pages/Job.jsx
@@ -1,5 +1,80 @@
+import { getSingleJob } from "@/api/apiJobs";
+import useFetch from "@/hooks/useFetch";
+import { useUser } from "@clerk/clerk-react";
+import MDEditor from "@uiw/react-md-editor";
+import { Briefcase, DoorClosed, DoorOpen, MapPinIcon } from "lucide-react";
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { BarLoader } from "react-spinners";
+
 const Job = () => {
-  return <div>Job</div>;
+  const { user, isLoaded } = useUser();
+  const { id } = useParams();
+
+  const {
+    loading: loadingJob,
+    data: job,
+    fetchData: fetchJob,
+  } = useFetch(getSingleJob, { job_id: id });
+
+  useEffect(() => {
+    if (isLoaded) fetchJob();
+  }, [isLoaded]);
+
+  console.log(job);
+
+  if (!isLoaded || loadingJob) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+  return (
+    <div className="flex flex-col gap-8 mt-5">
+      <div className="flex flex-col-reverse gap-6 md:flex-row justify-between items-center">
+        <h1 className="gradient-title font-extrabold pb-3 text-4xl sm:text-6xl">
+          {job?.title}
+        </h1>
+        <img
+          src={job?.company?.logo_url}
+          className="h-12"
+          alt={job?.title}
+        ></img>
+      </div>
+
+      <div className="flex justify-between">
+        <div className="flex gap-2">
+          <MapPinIcon />
+          {job?.location}
+        </div>
+        <div className="flex gap-2">
+          <Briefcase />
+          {job?.applications?.length} Applicants
+        </div>
+        <div className="flex gap-2">
+          {job?.isOpen ? (
+            <>
+              <DoorOpen /> Open
+            </>
+          ) : (
+            <>
+              <DoorClosed /> Closed
+            </>
+          )}
+        </div>
+      </div>
+
+      {/* hiring status */}
+      <h2 className="text-2xl sm:text-3xl font-bold">About the Job</h2>
+      <p>{job?.description}</p>
+
+      <h2 className="text-2xl sm:text-3xl font-bold">
+        What we are looking for
+      </h2>
+
+      <MDEditor.Markdown
+        source={job?.requirements}
+        className="bg-transparent sm:text-lg"
+      />
+    </div>
+  );
 };
 
 export default Job;
