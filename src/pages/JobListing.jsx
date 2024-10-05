7a8ef66d05001b32d3c6ec2bd239b161a01d4c02
code-walker-23 <chhabravinay549@gmail.com>
2024-10-05 10:59:37 +0530

    api-auth-completed-supabase-to-clerk

--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -1,4 +1,21 @@
+import { getJobs } from "@/api/apiAuth";
+import { useSession } from "@clerk/clerk-react";
+import { useEffect } from "react";
+
 const JobListing = () => {
+  const { session } = useSession();
+
+  const fetchJobs = async () => {
+    const supabaseAccessToken = await session.getToken({
+      template: "supabase",
+    });
+    console.log(supabaseAccessToken);
+    const data = await getJobs(supabaseAccessToken);
+    console.log(data);
+  };
+  useEffect(() => {
+    fetchJobs();
+  }, []);
   return <div>JobListing</div>;
 };
 
