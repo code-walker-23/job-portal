6dffdb2674a9929181a79da8302e4bd683df04b1
code-walker-23 <chhabravinay549@gmail.com>
2024-10-05 13:32:01 +0530

    building-job-listing

--- src/pages/JobListing.jsx
+++ src/pages/JobListing.jsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import useFetch from "@/hooks/useFetch";
 import { useUser } from "@clerk/clerk-react";
 import { BarLoader } from "react-spinners";
+import JobCard from "@/components/JobCard";
 
 const JobListing = () => {
   const [location, setLocation] = useState("");
@@ -21,7 +22,28 @@ const JobListing = () => {
   if (!isLoaded) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
-  return <div>JobListing</div>;
+  return (
+    <div>
+      <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl lg:text-8xl text-center pb-8">
+        Latest Jobs
+      </h1>
+      {loading ? (
+        <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />
+      ) : (
+        ""
+      )}
+
+      {loading === false && (
+        <div className="mt-8 grid md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {data?.length ? (
+            data.map((data) => <JobCard key={data.id} job={data} />)
+          ) : (
+            <div>No Jobs Found</div>
+          )}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default JobListing;
